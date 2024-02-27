e9cc980984c5ec518e5c897714de95d45c36f1a8
unknown <eskicmihajlo55@gmail.com>
2024-02-27 23:30:50 +0100

    added some more functions and regex

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
+const infixToFunction = {
+
+    "x" : (x,y) => x+y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+
+}
+
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1,operator, arg2)=> infixToFunction[operator](parseFloat(arg1),parseFloat(arg2)))
+
+const highPrecedence = (str) =>{
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str,regex);
+    return str === str2 ? str : highPrecedence(str2);
+
+
+
+
+
+    
+}
+
+
+
 const sum = (nums)=> nums.reduce((acc,cur)=> acc+cur,0)
 
 const isEven = (num)=> num % 2 ===0;
@@ -19,15 +44,33 @@ const median = (nums)=> {
 
 
 
+
+
 const spreadsheetFunctions = {
 
 sum,
 average,
 median ,
+even: nums => nums.filter(isEven),
+firsttwo : nums => nums.slice(0,2),
+has2 : nums =>  nums.includes(2),
+increment : nums => nums.map((num)=> num +1),
 
 
 
 
+}
+
+
+const applyFunction = (str) => {
+
+    const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = args => args.split(",").map(parseFloat);
+    const apply = (fn,args) =>spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    return str2.replace(functionCall, (match, fn,args)=> spreadsheetFunctions.hasOwnPropert(fn.toLowerCase()))
 }
 
 
@@ -44,7 +87,12 @@ const evalFormula  = (x , cells) => {
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
     const rangeFromString = (num1,num2) => range(parseInt(num1),parseInt(num2));
     const elemValue = num => character => idToText(character + num)
-    const addCharacters = character1 => character2 => num =>  charRange(character1, character2).map()
+    const addCharacters = character1 => character2 => num =>  charRange(character1, character2).map(elemValue(num))
+    const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2)=> rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+    const cellRegex = /[A-J][1-9][0-9]?/gi;
+    const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()))
+    const functionExpanded = applyFunction(cellExpanded);
+    return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
 
@@ -107,6 +155,7 @@ const update = (event) =>
     const value= element.value.replace(/\s/g, "");
     if(!value.includes(element.id) && value.charAt(0) == "=" ) 
     {
+        element.value = evalFormula(value.slice(1),Array.from(document.getElementById("container").children) )
 
     }
 
